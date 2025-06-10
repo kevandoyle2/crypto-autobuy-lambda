4a1eabfb6ea6968752ef80ee40c9e2f98f87a75a
Kevan Doyle <kpd@duck.com>
2025-06-10 18:39:45 -0400

    Refactor GeminiClient implementation and update CloudFormation template to use shared layer for code reuse

--- recurring-buy-eth/function.py
+++ recurring-buy-eth/function.py
@@ -1,6 +1,6 @@
 import json
 import boto3
-from shared.gemini_client import GeminiClient
+from shared.python.gemini_client import GeminiClient
 
 # Initialize SSM client
 ssm_client = boto3.client('ssm')
